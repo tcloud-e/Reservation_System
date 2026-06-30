6e48dd66b3d8baccc18e2f87c9d7f8f91a32abf9
230574 <te230574@gmail.com>
2026-06-30 10:13:00 +0900

    p

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "hey guys!" );
+        System.out.println( "hey guys we have a gift for you!" );
     }
 }
